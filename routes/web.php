84e535b8554a7894851ae4aefbd0d5c9d198d9fa
SONOE385 <kotarou385@gmail.com>
2022-06-27 17:55:16 +0900

    Web.php 追加

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+    //httpsのCSSを読み込ませるために記述
     if (config('app.env') === 'production') {
         URL::forceScheme('https');
     }
